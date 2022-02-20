2fbc6da396d64aa14cf0480c5d9a8b1668076d40
mentesniker <35209340+mentesniker@users.noreply.github.com>
2022-02-19 19:30:18 -0600

    Add files via upload

---
+++ front/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Formulario/>
+  </div>
+</template>
+
+<script>
+import Formulario from './components/Formulario.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Formulario
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ front/src/components/Formulario.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <label>Dime tu nombre</label>
+    <input type="text" placeholder="dime tu nombre" v-model="nombre"/>
+    <p>Tu nombre es: {{nombre}}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Formulario',
+  data () {
+      return {
+          nombre: "juan"
+      }
+  }
+}
+</script>
